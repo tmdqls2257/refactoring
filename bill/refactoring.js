9a552ca441434aeb9db971ed78ed615f061fd513
tmdqls2257 <54207332+tmdqls2257@users.noreply.github.com>
2021-06-17 00:13:03 +0900

    const__inline

--- bill/refactoring.js
+++ bill/refactoring.js
@@ -1,3 +1,29 @@
+function statement(invoice, plays){
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `청구 내역 (고객명: ${invocie.cutomer})\n`;
+  const format = new Intl.NumberFormat("en-US",{style: "currency", currency: "USD", minimumFractionDigits: 2}).format;
+  for (let perf of invoice.performances){
+    // const play = playFor(perf); 인라인된 변수는 제거
+    let thisAmount = amountFor(perf, playFor(perf));
+    // 포인트를 적립한다.
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    // 희슷 관객 5명마다 추가 포인트를 제공
+    if ("comedy" === playFor(perf).type) volumeCredits +=Math.floor(perf.audience / 5);
+    // play를 playFor로 변경한다.
+    // 청구 내역을 출력한다.
+    result += `${playFor(perf).name}: ${format(thisAmount/100)} (${perf.audience}석)\n`;
+    totalAmount += thisAmount;
+  }
+  result +=`총액: ${format(totalAmount/100)}\n`;
+  result +=`적립 포인트: ${volumeCredits}점\n`;
+  return result;
+}
+
+function playFor(aPerformance){
+  return plays[aPerformance.playID];
+}
+
 function amountFor(perf, play){// 값이 바뀌지 않는 변수는 매개변수로 전달
   let thisAmount = 0;//변수를 초기화할 코드
   switch(play.type){
@@ -16,26 +42,4 @@ function amountFor(perf, play){// 값이 바뀌지 않는 변수는 매개변수
     default: throw new Error(`알 수 없는 장르: ${play.type}`);
   }
   return thisAmount;
-  }
-  
-  function statement(invoice, plays){
-    let totalAmount = 0;
-    let volumeCredits = 0;
-    let result = `청구 내역 (고객명: ${invocie.cutomer})\n`;
-    const format = new Intl.NumberFormat("en-US",{style: "currency", currency: "USD", minimumFractionDigits: 2}).format;
-    for (let perf of invoice.performances){
-      const play = plays[perf.playID];
-      let thisAmount = amountFor(perf, play);
-      // 포인트를 적립한다.
-      volumeCredits += Math.max(perf.audience - 30, 0);
-      // 희슷 관객 5명마다 추가 포인트를 제공
-      if ("comedy" ===play.type) volumeCredits +=Math.floor(perf.audience / 5);
-  
-      // 청구 내역을 출력한다.
-      result += `${play.name}: ${format(thisAmount/100)} (${perf.audience}석)\n`;
-      totalAmount += thisAmount;
-    }
-    result +=`총액: ${format(totalAmount/100)}\n`;
-    result +=`적립 포인트: ${volumeCredits}점\n`;
-    return result;
   }
