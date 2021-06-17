490546a005f7cb328a864c4adc906fb07f8da41c
tmdqls2257 <tmdqls2257@naver.com>
2021-06-17 15:24:35 +0900

    totalAmount_inline

--- bill/refactoring.js
+++ bill/refactoring.js
@@ -1,15 +1,21 @@
 function statement(invoice, plays){
-  let totalAmount = 0;
   let result = `청구 내역 (고객명: ${invocie.cutomer})\n`;
   for (let perf of invoice.performances){
 
     // 청구 내역을 출력한다.
     result += `${playFor(perf).name}: ${USD(thisAmount/100)} (${perf.audience}석)\n`;
-    totalAmount += amountFor(perf);
   }
-  result +=`총액: ${USD(totalAmount/100)}\n`;
+  result +=`총액: ${USD(appleSauce()/100)}\n`;
   result +=`적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
+}
+
+function appleSauce(){
+  let totalAmount = 0;
+  for (let perf of invoice.performances){
+    totalAmount += amountFor(perf);
+  }
+  return totalAmount;
 }
 
 function totalVolumeCredits(){
