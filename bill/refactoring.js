7fae434843dd78949c1190053ceee4a14bab9475
tmdqls2257 <tmdqls2257@naver.com>
2021-06-17 15:10:22 +0900

    refactoring_volumCredits

--- bill/refactoring.js
+++ bill/refactoring.js
@@ -1,45 +1,59 @@
 function statement(invoice, plays){
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invocie.cutomer})\n`;
-  const format = new Intl.NumberFormat("en-US",{style: "currency", currency: "USD", minimumFractionDigits: 2}).format;
   for (let perf of invoice.performances){
-    volumeCredits += volumeCreditsFor(perf);
-    result += `${playFor(aPerformance).name}: ${format(thisAmount/100)} (${perf.audience}석)\n`;
+
+    // 청구 내역을 출력한다.
+    result += `${playFor(perf).name}: ${USD(thisAmount/100)} (${perf.audience}석)\n`;
     totalAmount += amountFor(perf);
   }
-  result +=`총액: ${format(totalAmount/100)}\n`;
-  result +=`적립 포인트: ${volumeCredits}점\n`;
+  result +=`총액: ${USD(totalAmount/100)}\n`;
+  result +=`적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
 }
 
-function volumeCreditsFor(perf){
+function totalVolumeCredits(){
   let volumeCredits = 0;
-  volumeCredits += Math.max(perf.audience - 30, 0);
-  if ("comedy" === playFor(aPerformance).type) volumeCredits +=Math.floor(perf.audience / 5);
+  for (let perf of invoice.performances){
+    volumeCredits += volumeCreditsFor(perf);
+  }
   return volumeCredits;
 }
 
-function playFor(aPerformance){
-  return plays[aPerformance.playID];
+
+
+function USD(aNumber){
+  return new Intl.NumberFormat("en-US",{style: "currency", currency: "USD", minimumFractionDigits: 2}).format(aNumber);
 }
 
-function amountFor(perf){// 값이 바뀌지 않는 변수는 매개변수로 전달
-  let thisAmount = 0;//변수를 초기화할 코드
-  switch(playFor(aPerformance).type){
+function volumeCreditsFor(aPerformance){
+  let result = 0;
+  // 포인트를 적립한다.
+  result += Math.max(aPerformance.audience - 30, 0);
+  // 희슷 관객 5명마다 추가 포인트를 제공
+  if ("comedy" === playFor(aPerformance).type) result +=Math.floor(aPerformance.audience / 5);
+}
+
+function playFor(perf){
+  return plays[perf.playID];
+}
+
+function amountFor(aPerformance){// 값이 바뀌지 않는 변수는 매개변수로 전달
+  let result = 0;//변수를 초기화할 코드
+  switch(playFor(perf).type){
     case "tragedy": //비극
-      thisAmount = 40000;
-      if (perf.audience > 30){
-        thisAmount +=1000 * (perf.audience - 30);
+      result = 40000;
+      if (aPerformance.audience > 30){
+        result +=1000 * (aPerformance.audience - 30);
       }
     case "comedy": //희극
-      thisAmount = 30000;
-      if (perf.audience > 20){
-        thisAmount += 10000 + 500 * (perf.audience - 20);
+      result = 30000;
+      if (aPerformance.audience > 20){
+        result += 10000 + 500 * (aPerformance.audience - 20);
       }
-      thisAmount += 300 * perf.audience;
+      result += 300 * aPerformance.audience;
       break;
-    default: throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+    default: throw new Error(`알 수 없는 장르: ${playFor(perf).type}`);
   }
-  return thisAmount;
+  return result;
   }
