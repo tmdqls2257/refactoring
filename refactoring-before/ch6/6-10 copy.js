b4ac7ce5d9f609df8d47ac6ea9d16b13cb582e57
Seung Bin Hong <tmdqls2257@gmail.com>
2022-07-09 23:02:56 +0900

    refactoring 6-10

---
+++ refactoring-before/ch6/6-10 copy.js
@@ -0,0 +1,25 @@
+import _ from 'lodash'
+
+const reading = { customer: 'ivan', quantity: 10, month: 5, year: 2017 }
+
+export function acquireReading() {
+  return reading
+}
+
+export function enrichReading(original) {
+  const result = _.cloneDeep(original)
+  result.baseCharge = calculateBaseCharge(result)
+  result.taxableCharge = Math.max(
+    0,
+    result.baseCharge - taxThreshold(result.year)
+  )
+}
+
+function calculateBaseCharge(reading) {
+  return baseRate(reading.month, reading.year) * reading.quantity
+}
+
+export function baseRate(month, year) {
+  if (year === 2017 && month === 5) return 0.1
+  return 0.2
+}
---
+++ refactoring-before/ch6/6-10-client1 copy.js
@@ -0,0 +1,7 @@
+import { acquireReading, enrichReading } from './6-10.js'
+
+const rawReading = acquireReading()
+const reading = enrichReading(rawReading)
+
+const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity
+console.log(baseCharge)
